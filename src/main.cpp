06345461fddd1bbc9eabd3427f765e332a4de780
flinger-bit <xdffsxdxd@gmail.com>
2026-04-01 13:33:18 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@ using namespace geode::prelude;
 
 class $modify(MyGameManager, GameManager) {
     void setFPS(float fps) {
-        if (Mod::get()->getSettingValue<bool>("bypass")) {
-            fps = Mod::get()->getSettingValue<int>("fps");
+        auto mod = Mod::get();
+        if (mod && mod->getSettingValue<bool>("bypass")) {
+            fps = static_cast<float>(mod->getSettingValue<int64_t>("fps"));
         }
         GameManager::setFPS(fps);
     }
@@ -17,14 +18,17 @@ class $modify(MyPlayer, PlayerObject) {
     void update(float dt) {
         PlayerObject::update(dt);
 
-        if (Mod::get()->getSettingValue<bool>("ship") && this->m_isShip) {
-            this->m_rotation = 0;
-            this->m_velocity.y = 0;
+        auto mod = Mod::get();
+        if (!mod) return;
+
+        if (mod->getSettingValue<bool>("ship") && this->m_isShip) {
+            this->m_rotation = 0.0f;
+            this->m_velocity.y = 0.0f;
         }
 
-        if (Mod::get()->getSettingValue<bool>("wave") && this->m_isBird) {
-            this->m_rotation = 0;
-            this->m_velocity.y = 0;
+        if (mod->getSettingValue<bool>("wave") && this->m_isBird) {
+            this->m_rotation = 0.0f;
+            this->m_velocity.y = 0.0f;
         }
     }
 };
